4407158fca6b219144b5f7c2d6343926c2bedfe1
alecruste <algirdas.rusteika@reactseals.com>
2019-09-11 13:33:34 +0300

    removed unnecessary commented code

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform, Text, View, TouchableOpacity, FlatList, Image, ScrollView } from 'react-native';
+import { Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
 import styles from './src/styles';
 import Header from './src/components/Header';
 import Footer from './src/components/Footer';
@@ -60,58 +52,6 @@ Test
             <View style={styles.main}>
                 <Header />
                 {!isLoading ? (
-                //                     <FlatList
-                //                         data={radioStationList}
-                //                         showsVerticalScrollIndicator={false}
-                //                         renderItem={({ item }) => (
-                //                             <View>
-                //                                 <TouchableOpacity onPress={() => this.handleRadioStation(item)}>
-                //                                     <View style={styles.radioStationList}>
-                //                                         <Text style={styles.radioStationName} numberOfLines={1}>
-                //                                             {item.name}
-                //                                             {' '}
-                // FM
-                //                                         </Text>
-                //                                         <Text style={styles.radioStationFrequency} numberOfLines={1}>
-
-                //                                             {item.votes}
-                // ,
-                //                                             {' '}
-                //                                             {item.votes[0]}
-                //                                         </Text>
-                //                                     </View>
-                //                                     {/* {radioStationActive && (
-                //                                         <View style={styles.radioStationDetails}>
-                //                                             <TouchableOpacity>
-                //                                                 <Image
-                //                                                     style={{ width: 35, height: 35 }}
-                //                                                     source={require('./src/assets/minus.png')}
-                //                                                 />
-                //                                             </TouchableOpacity>
-                //                                             {item.favicon === '' ? (
-                //                                                 <Image
-                //                                                     style={styles.radioStationLogo}
-                //                                                     source={require('./src/assets/radioFallBackImage.jpg')}
-                //                                                 />
-                //                                             ) : (
-                //                                                 <Image
-                //                                                     style={styles.radioStationLogo}
-                //                                                     source={{ uri: `${item.favicon}` }}
-                //                                                 />
-                //                                             )}
-                //                                             <TouchableOpacity>
-                //                                                 <Image
-                //                                                     style={{ width: 35, height: 35 }}
-                //                                                     source={require('./src/assets/plus.png')}
-                //                                                 />
-                //                                             </TouchableOpacity>
-                //                                         </View>
-                //                                     ) } */}
-                //                                 </TouchableOpacity>
-                //                             </View>
-                //                         )}
-                //                         keyExtractor={item => item.id}
-                //                     />
                     <ScrollView>
                         {radioStationList.map(radioStation => (
                             <View key={radioStation.id}>
